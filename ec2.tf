0c3697c2126f2f372a2ee78cddb9b2794063ce5e
mangesh85964 <157485119+mangesh85964@users.noreply.github.com>
2024-02-03 16:30:12 +0530

    Remove tags

--- ec2.tf
+++ ec2.tf
@@ -5,10 +5,6 @@ provider "aws" {
 resource "aws_instance" "ec2" {
  ami = "ami-0d63de463e6604d0a"
  instance_type = "t2.micro"
-
- tags = {
-  Name = "terraform-ins"
- }
 }
 
 resource "aws_eip" "eip" {
